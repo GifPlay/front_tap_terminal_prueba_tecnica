5e32c4b76e91a112da205b0d1d48a2e18fbe24c5
GifPlay <ioa_isma@hotmail.com>
2025-12-04 13:50:29 -0600

    add rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login';
+import { ProductosComponent } from './components/productos/productos';
+import { Layaout } from './components/layaout/layaout';
+import { IndexUsuarios } from './components/usuarios/index-usuarios/index-usuarios';
+import { PerfilesComponent } from './components/perfiles/perfiles';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: LoginComponent },
+  {
+    path: '',
+    component: Layaout,
+    children: [
+      { path: 'productos', component: ProductosComponent },
+      { path: 'usuarios', component: IndexUsuarios },
+      { path: 'perfiles', component: PerfilesComponent },
+    ]
+  },
+  
+];
